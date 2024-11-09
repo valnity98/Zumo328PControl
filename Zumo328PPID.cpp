a9ff132743e79e6393b73e160141b3331c79f2e6
MutasemBader <127694032+valnity98@users.noreply.github.com>
2024-11-09 20:25:29 +0100

    Kleine Verbesserung

--- Zumo328PPID.cpp
+++ Zumo328PPID.cpp
@@ -3,20 +3,20 @@
 
 Zumo328PPID::Zumo328PPID(float maxSpeed) : maxSpeed(maxSpeed), leftSpeed(0), rightSpeed(0) {}
 
-void Zumo328PPID::ControlSpeed(uint16_t& measured_position, const uint16_t target_position, float kp, float kd){
+void Zumo328PPID::ControlSpeed(uint16_t& measured_position, const uint16_t target_position, float kp, float kd, float deltaT, bool aktiv){
 
 // Our "error" is how far we are away from the center of the line, which corresponds to target_position.
 int16_t error = measured_position - target_position;
 
 // Get motor speed difference using proportional and derivative PID terms (the integral term is generally not very useful for line following).  
-
-//currenttime in micorseconds
+if(aktiv){
+    //currenttime in micorseconds
 long currT = micros(); 
 
 //deltaT in seconds
-float deltaT = ((float) (currT-prevT))/ 1.0e6; 
+deltaT = ((float) (currT-prevT))/ 1.0e6; 
 prevT = currT;
-
+}
 
 //Control Signal (motor speed difference)
 int32_t speedDifference = (int32_t)(kp * error)+(int32_t)(kd * ((error - lastError) / deltaT));
