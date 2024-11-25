99470e09a5eeb48247e7acfb53095cc0ef304ad4
MutasemBader <127694032+valnity98@users.noreply.github.com>
2024-11-25 14:54:03 +0100

    Update Zumo328PEncoders.h

--- Zumo328P Library/Zumo328PEncoders.h
+++ Zumo328P Library/Zumo328PEncoders.h
@@ -32,20 +32,20 @@ public:
      * The count is returned as a signed 16-bit integer.  When the count goes
      * over 32767, it will overflow down to -32768.  When the count goes below
      * -32768, it will overflow up to 32767. */
-    static int16_t getCountsLeft();
+    static int32_t getCountsLeft();
 
     /*! This function is just like getCountsLeft() except it applies to the
      *  right-side encoder. */
-    static int16_t getCountsRight();
+    static int32_t getCountsRight();
 
     /*! This function is just like getCountsLeft() except it also clears the
      *  counts before returning.  If you call this frequently enough, you will
      *  not have to worry about the count overflowing. */
-    static int16_t getCountsAndResetLeft();
+    static int32_t getCountsAndResetLeft();
 
     /*! This function is just like getCountsAndResetLeft() except it applies to
      *  the right-side encoder. */
-    static int16_t getCountsAndResetRight();
+    static int32_t getCountsAndResetRight();
 
 
 private:
